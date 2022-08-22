745336cb4715b30e8441dc80877117b93830bf53
Johnathan <johnathan.mnn@gmail.com>
2022-08-22 10:55:41 -0500

    Added mailto function, fixed minor bugs

--- index.js
+++ index.js
@@ -21,11 +21,6 @@ const managerQuestions = [
         name: 'managerName',
       },
       {
-        type: 'input',
-        message: 'What is the managers title?',
-        name: 'managerTitle',
-      },
-      {
           type: 'input',
           message: 'What is the managers ID number?',
           name: 'managerId',
@@ -105,7 +100,10 @@ function manager(){
     return inquirer.prompt(managerQuestions)
     // After questions have been answered the manager card wll be appended to the html file
     .then((answers) => {
-      fs.appendFile("dist/index.html", '<div class="card mx-4 my-4" style="width: 15rem;"><div class="card-body"><h5 class="card-title">'+answers.managerName+'</h5><h6 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-briefcase"></i> Manager</h6><p class="card-text">ID:'+answers.managerId+' </p><p class="card-text">Email:'+answers.managerEmail+'</p><p class="card-text">Office:'+answers.managerOffice+' <a href="#" class="card-link">Card link</a> </p></div></div>' , (err) => { 
+      // Generates head of document adn then title of page
+      generateHead();
+      // Appends manager info to page
+      fs.appendFile("dist/index.html", '<div class="card mx-4 my-4" style="width: 15rem;"><div class="card-body"><h5 class="card-title">'+answers.managerName+'</h5><h6 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-briefcase"></i> Manager</h6><p class="card-text">ID:'+answers.managerId+' </p><p class="card-text">Email:<a href = "mailto:'+answers.managerEmail+'">'+answers.managerEmail+'</a></p><p class="card-text">Office:'+answers.managerOffice+' </p></div></div>' , (err) => { 
         if (err) { 
           console.log(err); 
         } 
@@ -120,24 +118,24 @@ function manager(){
           if (response.employeeType === "Engineer" ) {
               // After questions have been answered the engineer card wll be appended to the html file as many times as requested by the user
             return inquirer.prompt(engineerQuestions).then((answers) => {
-              fs.appendFile("dist/index.html", '<div class="card mx-4 my-4" style="width: 15rem;"><div class="card-body"><h5 class="card-title">'+answers.engineerName+'</h5><h6 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-computer"></i>Engineer</h6><p class="card-text">ID:'+answers.engineerId+' </p><p class="card-text">Email:'+answers.engineerEmail+'</p><p class="card-text">Office:'+answers.managerOffice+' <a href="https://github.com/'+answers.engineerGithub+'" class="card-link">Github:'+answers.engineerGithub+'</a></div></div>' , (err) => { 
+              fs.appendFile("dist/index.html", '<div class="card mx-4 my-4" style="width: 15rem;"><div class="card-body"><h5 class="card-title">'+answers.engineerName+'</h5><h6 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-computer"></i>Engineer</h6><p class="card-text">ID:'+answers.engineerId+' </p><p class="card-text">Email:<a href = "mailto:'+answers.engineerEmail+'">'+answers.engineerEmail+'</a></p><p class="card-text">Office:'+answers.managerOffice+'<p class="card-text">Github: <a href="https://github.com/'+answers.engineerGithub+'" class="card-link">'+answers.engineerGithub+'</a></p></div></div>' , (err) => { 
                 if (err) {console.log(err); } });
                 if (answers.addEngineer) {return getAnswers();}})}
           // Allows the asking of the intern questions and then the looping of questions if desired        
             if (response.employeeType === "Intern") {
               // After questions have been answered the intern card wll be appended to the html file as many times as requested by the user
               return inquirer.prompt(internQuestions).then((answers)=>{
-              fs.appendFile("dist/index.html", '<div class="card mx-4 my-4" style="width: 15rem;"><div class="card-body"><h5 class="card-title">'+answers.internName+'</h5><h6 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-graduation-cap"></i>Intern</h6><p class="card-text">ID:'+answers.internId+' </p><p class="card-text">Email:'+answers.internEmail+'</p> <p class="card-text">Email:'+answers.internSchool+'</p>' , (err) => { 
+              fs.appendFile("dist/index.html", '<div class="card mx-4 my-4" style="width: 15rem;"><div class="card-body"><h5 class="card-title">'+answers.internName+'</h5><h6 class="card-subtitle mb-2 text-muted"><i class="fa-solid fa-graduation-cap"></i>Intern</h6><p class="card-text">ID:'+answers.internId+' </p><p class="card-text">Email:<a href = "mailto:'+answers.internEmail+'">'+answers.internEmail+'</a></p> <p class="card-text">School:'+answers.internSchool+'</p>' , (err) => { 
               if (err) { 
               console.log(err); } });
               if (answers.addIntern) {return getAnswers();}})}
-              if (response.employeeType === "No More!") {}})};
-  // Generates start of html document with generateHead() then asks and prints manager questions same with engineer/intern, and finally the scripts and bottom of page with generateBottom()
-  generateHead();
+              // After the user is done answering questions the bottom of the document will be appended
+              if (response.employeeType === "No More!") {
+                generateBottom();
+              }})};
+  // Asks manager questions and then asks intern engineer questions
   manager().then (()=>{
-    getAnswers().then (()=>{
-      generateBottom();
-    })
+    getAnswers()
   });
 
 
--- src/createHead.js
+++ src/createHead.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-var head = "<!DOCTYPE html><html><head><title>Development Team</title><meta charset='UTF-8' /><meta name='viewport' content='width=device-width, initial-scale=1' /><!-- Bootstrap stylesheet --><link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'><!-- Local Stylesheet --><link 'rel='stylesheet' type='text/css' href='styles.css'></head><body><header><h1 class='text-center p-5' style='background-color: rgb(116, 163, 234); color: #FFF'>Development Team</h1></header><section><div class='row'>";
+var head = "<!DOCTYPE html><html><head><title>Development Team</title><meta charset='UTF-8' /><meta name='viewport' content='width=device-width, initial-scale=1' /><!-- Bootstrap stylesheet --><link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'></head><body><header><h1 class='text-center p-5' style='background-color: rgb(116, 163, 234); color: #FFF'>Development Team</h1></header><section><div class='row'>";
 
 function generateHead (){
     fs.appendFile("./dist/index.html", head , (err) => { 
